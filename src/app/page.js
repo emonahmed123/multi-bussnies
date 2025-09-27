19c55e342c3617c050e48e91686268aa5b405476
Emon Ahmed <imonshomon@gmail.com>
2025-09-27 09:47:20 +0600

    desing card

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import HeroSection from "@/components/ui/homePage/HeroSection";
+import HowtoWork from "@/components/ui/homePage/HowtoWork";
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
 
       <HeroSection />
 
-      {/* <HowtoWork /> */}
+      <HowtoWork />
     </div>
   );
 }
--- src/components/ui/homePage/HowtoWork.js
+++ src/components/ui/homePage/HowtoWork.js
@@ -1,15 +1,16 @@
-
+import Image from "next/image";
+import Booking from "../../../../public/booking.svg";
 const HowtoWork = () => {
     return (
-        <section className="maw-w-[1200px] mx-auto px-3 xl:px-0 py-10   ">
+        <section className="max-w-[1200px] mx-auto px-3 xl:px-0 py-10 font-public-sans border-primary-200   ">
 
 
-            <div className="w-[284px] border-r ">
-                <Image src={""} />
+            <div className="w-[284px] border-r border-text-200 ">
+                <Image src={Booking} alt="Easy Service Booking" width={43} height={43} className="mb-4" />
 
-                <h6>Easy Service Booking</h6>
+                <h6 className="text-[18px] leading-[28px] font-semibold mb-2 ">Easy Service Booking</h6>
 
-                <p className="max-w-[221px]">Streamlined booking process for clients with service catalogs and availability.</p>
+                <p className="max-w-[221px] text-[14px] leading-[24px] text-text-600">Streamlined booking process for clients with service catalogs and availability.</p>
 
             </div>
 
